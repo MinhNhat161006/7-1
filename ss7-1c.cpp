1fd7c104d69ede8cc59bce19815ddd231ad315cd
MinhNhat161006 <n24dtcn060@student.ptithcm.edu.vn>
2024-12-18 14:12:18 +0700

    Add files via upload

---
+++ ss7-1c.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int length = 5;
+
+    for (int i = 0; i < length; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    printf("%d\n", length);
+
+    return 0;
+}
